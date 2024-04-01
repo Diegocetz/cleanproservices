bbd0ead8c2c41e12f99eb20c07b8588f14fcd0dc
Diegocetz <cetzdominguezdiegoyahir@gmail.com>
2024-04-01 10:27:51 -0600

    cambio de estilos

--- application/views/secciones/header.php
+++ application/views/secciones/header.php
@@ -1,10 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<header>
+<head>
   <title>Clean Pro Services</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  
+   <!--Bootstrap-->
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+ <!--css-->
+ <link href="assets\css\estilos.css" rel="stylesheet" type="text/css">
 
   <!--icons-->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -13,22 +23,15 @@
   <!--ajax-->
   <script src="<?=base_url()?>assets\js\jquery.min.js"></script>
 
-
-  <!--js-->
-  <script src="<?=base_url()?>assets\js\bootstrap.min.js"></script>
-  
   <!--Fonts-->
   <link href="<?=base_url()?>assets\css\Poppins.css" rel="stylesheet">
   <link href="<?=base_url()?>assets\css\Montserrat.css" rel="stylesheet">
-  <link href="<?=base_url()?>assets\css\Raleway.css" rel="stylesheet">
+  <link href="<?=base_url()?>assets\css\Raleway.css" rel="stylesheet">  
 
-  <!--Bootstrap-->
-  <link href="<?=base_url()?>assets\css\bootstrap.min.css" rel="stylesheet">
+  
+ 
 
-  <!--css-->
-  <link href="<?=base_url()?>assets\css\style.css" rel="stylesheet">
-  
-  
+</head>
 
 
 
